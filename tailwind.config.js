98ccebe55f7358825bd98a2a3a540643bd6508d6
vishcomestrue <ramvishwa03@gmail.com>
2021-07-10 22:38:48 +0530

    Updated config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    height: {
+      24: '6rem',
+      half: '50vh',
+      threeFourth: '75vh'
+    },
   },
   variants: {
     extend: {},
